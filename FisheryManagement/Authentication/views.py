bdcb05decf62b24d3f690357b6af8fb698bd796a
gemrey <gemreyranola@gmail.com>
2023-06-04 14:30:34 +0800

    Fixing All Error

--- FisheryManagement/Authentication/views.py
+++ FisheryManagement/Authentication/views.py
@@ -2,17 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def  index(request):
+def index(request):
     return HttpResponse('<h1>my first app</h1>')
 
-def  ishtml(request):
+def login(request):
    return render(request, 'index.html')
 
-def  isreghtml(request):
+def isreghtml(request):
    return render(request, 'register.html')
 
-def  ispmloginhtml(request):
+def ispmloginhtml(request):
    return render(request, 'PManager.html')
 
-def  ispmreghtml(request):
+def ispmreghtml(request):
    return render(request, 'PMregister.html')
