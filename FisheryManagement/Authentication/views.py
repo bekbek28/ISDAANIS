53e3ec933567ac75ce7178499452308ed8d9e2b2
bekbek28 <111419395+bekbek28@users.noreply.github.com>
2023-06-06 22:50:39 +0800

    login() takes 1 positional argument but 2 were given

--- FisheryManagement/Authentication/views.py
+++ FisheryManagement/Authentication/views.py
@@ -1,18 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
-def index(request):
-   return HttpResponse('<h1>my first app</h1>')
+
+# Create your views here.
 
 def usertype(request):
    return render(request, 'usertype.html')
 
+def index(request):
+   return HttpResponse('<h1>my first app</h1>')
+
+
 def login(request):
+   if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('/login/')    
    return render(request, 'index.html')
 
 def isreghtml(request):
-   return render(request, 'register.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        
+        if password == confirm_password:
+            User.objects.create_user(username=username, password=password)
+            return redirect('/login/')  # Replace with the desired URL for login
+    
+    return render(request, 'register.html')
 
 def ispmloginhtml(request):
    return render(request, 'PManager.html')
