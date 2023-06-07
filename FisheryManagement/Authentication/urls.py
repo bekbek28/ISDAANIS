e7d059a178440f3cbb2092faa558ea08a13a1b8f
bekbek28 <111419395+bekbek28@users.noreply.github.com>
2023-06-07 21:10:45 +0800

    may function na

--- FisheryManagement/Authentication/urls.py
+++ FisheryManagement/Authentication/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('homepage/', views.index),
-    path('usertype/' , views.usertype, name='usertype'),
-    path('login/' , views.login, name='login'),
+    path('' , views.usertype, name='usertype'),
+    path('login/' , views.islogin, name='login'),
     path('register/' , views.isreghtml, name='register'),
     path('pmlogin/' , views.ispmloginhtml, name='pmlogin'),
     path('pmreg/' , views.ispmreghtml, name='pmregister'),
--- FisheryManagement/Authentication/views.py
+++ FisheryManagement/Authentication/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db import IntegrityError
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login 
 from django.contrib.auth.models import User
 
 
+
 # Create your views here.
+@login_required(login_url='login/')
 
 def usertype(request):
    return render(request, 'usertype.html')
@@ -14,31 +18,70 @@ def index(request):
    return HttpResponse('<h1>my first app</h1>')
 
 
-def login(request):
-   if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+def islogin(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('pass')
         user = authenticate(request, username=username, password=password)
         
         if user is not None:
             login(request, user)
-            return redirect('/login/')    
-   return render(request, 'index.html')
+            return redirect('forms/')
+        else:
+            return HttpResponse("Username or Password is incorrect!!!")
+
+    return render(request, 'index.html')
 
 def isreghtml(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        confirm_password = request.POST['confirm_password']
-        
-        if password == confirm_password:
-            User.objects.create_user(username=username, password=password)
-            return redirect('/login/')  # Replace with the desired URL for login
-    
-    return render(request, 'register.html')
+        fullname = request.POST.get('fullname')
+        uname = request.POST.get('username')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
 
+        if password != confirm_password:
+            return HttpResponse("Your password and confirm password are not the same!!")
+        else:
+            try:
+                my_user = User.objects.create_user(uname, password)
+                my_user.save()
+                return redirect('login')
+            except IntegrityError:
+                return HttpResponse("Username or email already exists!")
+
+    return render(request, 'register.html')
+        
+        
 def ispmloginhtml(request):
+   if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('pass')
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard/')
+        else:
+            return HttpResponse("Username or Password is incorrect!!!")
+
+   
    return render(request, 'PManager.html')
 
 def ispmreghtml(request):
-   return render(request, 'PMregister.html')
+    if request.method == 'POST':
+        fullname = request.POST.get('fullname')
+        uname = request.POST.get('username')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+
+        if password != confirm_password:
+            return HttpResponse("Your password and confirm password are not the same!!")
+        else:
+            try:
+                my_user = User.objects.create_user(uname, password)
+                my_user.save()
+                return redirect('login')
+            except IntegrityError:
+                return HttpResponse("Username or email already exists!")
+
+    return render(request, 'PMregister.html')
