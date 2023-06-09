e174c81f983485982bcefd61dc99d59bbcda3ddf
bekbek28 <111419395+bekbek28@users.noreply.github.com>
2023-06-09 21:53:34 +0800

    okay na yung admin

--- FisheryManagement/Authentication/urls.py
+++ FisheryManagement/Authentication/urls.py
@@ -5,11 +5,11 @@ app_name = "Authentication"
 
 urlpatterns = [
     path('homepage/', views.index),
-    path('' , views.usertype, name='usertype'),
+    path('usertype' , views.usertype, name='usertype'),
     path('login/' , views.islogin, name='login'),
     path('register/' , views.isreghtml, name='register'),
     path('pmlogin/' , views.ispmloginhtml, name='pmlogin'),
     path('pmreg/' , views.ispmreghtml, name='pmregister'),
-    path('loginadmin/' , views.ispmloginhtml, name='loginadmin'),
+    path('loginadmin/' , views.isadminloginhtml, name='loginadmin'),
     path('regadmin/' , views.isadminreghtml, name='registeradmin'),
 ]
