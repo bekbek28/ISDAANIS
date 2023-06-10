7f81fe67399bfe53f7caacb49944258fd435cc9e
bekbek28 <111419395+bekbek28@users.noreply.github.com>
2023-06-10 15:03:23 +0800

    redirected too many times

--- FisheryManagement/Authentication/views.py
+++ FisheryManagement/Authentication/views.py
@@ -1,55 +1,48 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.db import IntegrityError
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login 
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required, user_passes_test
-
-
+from django.db import IntegrityError
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.models import Group
 
 # Create your views here.
+# Create the 'Market Checker' group if it doesn't exist
+market_checker_group, created = Group.objects.get_or_create(name='Market Checker')
+port_manager_group, created = Group.objects.get_or_create(name='Port Manager')
 
-# Custom decorator to check if the user is a Port Manager
-def port_manager_required(view_func):
-    def check_port_manager(user):
-        return user.is_authenticated and user.groups.filter(name='Port Manager').exists()
-    return user_passes_test(check_port_manager)(view_func)
+def redirect_based_on_role(user):
+    # Check if the user belongs to the 'Port Manager' group
+    if user.groups.filter(name='Authentication:pmregister').exists():
+        return redirect('Analytics:dashboard')  # Redirect to Port Manager dashboard
 
-# Custom decorator to check if the user is a Market Checker
-def market_checker_required(view_func):
-    def check_market_checker(user):
-        return user.is_authenticated and user.groups.filter(name='Market Checker').exists()
-    return user_passes_test(check_market_checker)(view_func)
+    # Check if the user belongs to the 'Market Checker' group
+    elif user.groups.filter(name='Authentication:register').exists():
+        return redirect('Analytics:forms')  # Redirect to Market Checker dashboard
 
-@login_required(login_url='login/')
+    else:
+        return HttpResponse("Invalid user role!")  # User has an unrecognized role
+
+@login_required(login_url='/login/')
 def usertype(request):
     return render(request, 'usertype.html')
 
-@port_manager_required
-def port_manager_view(request):
-    # Only accessible by users with the 'Port Manager' role
-    return HttpResponse("Port Manager view")
-
-@market_checker_required
-def market_checker_view(request):
-    # Only accessible by users with the 'Market Checker' role
-    return HttpResponse("Market Checker view")
-
 def index(request):
-    return HttpResponse('<h1>my first app</h1>')
+    return HttpResponse('<h1>My first app</h1>')
 
+@user_passes_test(lambda user: user.groups.filter(name='Market Checker').exists(), login_url='login/')
 def islogin(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
+        
         if user is not None:
             login(request, user)
-            return redirect('Analytics:forms')
+            return redirect_based_on_role(user)    # Update this with the correct URL
         else:
-            return HttpResponse("Username or Password is incorrect!!!")
+            return HttpResponse("Username or password is incorrect!")
 
     return render(request, 'index.html')
 
@@ -63,17 +56,23 @@ def isreghtml(request):
         confirm_password = request.POST.get('confirm_password')
 
         if password != confirm_password:
-            return HttpResponse("Your password and confirm password are not the same!!")
+            return HttpResponse("Your password and confirm password do not match!")
         else:
             try:
-                my_user = User.objects.create_user(uname, password)
+                my_user = User.objects.create_user(username=uname, password=password)
                 my_user.save()
+
+                # Assign the user to the 'Market Checker' group
+                market_checker_group = Group.objects.get(name='Market Checker')
+                market_checker_group.user_set.add(my_user)
+
                 return redirect('Authentication:login')
             except IntegrityError:
                 return HttpResponse("Username or email already exists!")
 
     return render(request, 'register.html')
 
+@user_passes_test(lambda user: user.groups.filter(name='Port Manager').exists(), login_url='/pmlogin/')
 def ispmloginhtml(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -82,11 +81,12 @@ def ispmloginhtml(request):
         
         if user is not None:
             login(request, user)
-            return redirect('Analytics:dashboard')
+            return redirect_based_on_role(user)   # Update this with the correct URL
         else:
-            return HttpResponse("Username or Password is incorrect!!!")
+            return HttpResponse("Username or password is incorrect!")
 
     return render(request, 'PManager.html')
+
 
 def ispmreghtml(request):
     if request.method == 'POST':
@@ -98,17 +98,23 @@ def ispmreghtml(request):
         confirm_password = request.POST.get('confirm_password')
 
         if password != confirm_password:
-            return HttpResponse("Your password and confirm password are not the same!!")
+            return HttpResponse("Your password and confirm password do not match!")
         else:
             try:
-                my_user = User.objects.create_user(uname, password)
+                my_user = User.objects.create_user(username=uname, password=password)
                 my_user.save()
-                return redirect('Authentication:pmlogin')
+
+                # Assign the user to the 'Port Manager' group
+                port_manager_group = Group.objects.get(name='Port Manager')
+                port_manager_group.user_set.add(my_user)
+
+                return redirect('Authentication:pmlogin')  # Update this with the correct URL
             except IntegrityError:
                 return HttpResponse("Username or email already exists!")
 
     return render(request, 'PMregister.html')
 
+@user_passes_test(lambda user: user.is_superuser, login_url='/login/')
 def isadminloginhtml(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -117,14 +123,13 @@ def isadminloginhtml(request):
         
         if user is not None:
             login(request, user)
-            return redirect('Analytics:admindashboard')
+            return redirect('Analytics:admindashboard')  # Update this with the correct URL
         else:
-            return HttpResponse("Username or Password is incorrect!!!")
+            return HttpResponse("Username or password is incorrect!")
 
     return render(request, 'admin.html')
 
-
-
+@user_passes_test(lambda user: user.is_superuser, login_url='/login/')
 def isadminreghtml(request):
     if request.method == 'POST':
         firstname = request.POST.get('fname')
@@ -135,12 +140,12 @@ def isadminreghtml(request):
         confirm_password = request.POST.get('confirm_password')
 
         if password != confirm_password:
-            return HttpResponse("Your password and confirm password are not the same!!")
+            return HttpResponse("Your password and confirm password do not match!")
         else:
             try:
-                my_user = User.objects.create_user(uname, password)
+                my_user = User.objects.create_user(username=uname, password=password)
                 my_user.save()
-                return redirect('Authentication:adminlogin')
+                return redirect('Authentication:loginadmin')  # Update this with the correct URL
             except IntegrityError:
                 return HttpResponse("Username or email already exists!")
 
