ac08886ee31f637d372705093c6b5a12d77ba53a
bekbek28 <111419395+bekbek28@users.noreply.github.com>
2023-07-06 07:15:42 +0000

    table modified

--- FisheryManagement/Analytics/static/js/analyticsTable.js
+++ FisheryManagement/Analytics/static/js/analyticsTable.js
@@ -1,18 +1,46 @@
- const ctx = document.getElementById('myChart');
 
-  new Chart(ctx, {
-    type: 'line',
+
+ const getRandomColors = (count) => {
+  const colors = [];
+  const letters = '0123456789ABCDEF';
+  for (let i = 0; i < count; i++) {
+    let color = 'rgba(';
+    for (let j = 0; j < 3; j++) {
+      color += Math.floor(Math.random() * 256) + ', ';
+    }
+    color += '0.5)';
+    colors.push(color);
+  }
+  return colors;
+};
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+const ctx = document.getElementById('Fishtype').getContext('2d');
+
+ new Chart(ctx, {
+    type: 'bar',
     data: {
       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
       datasets: [{
         label: '# of Votes',
         data: [12, 19, 3, 5, 2, 3],
+        borderColor: ['rgba(0, 255, 171, 0.8)', 
+        'rgba(0, 255, 171, 0.8)',
+        'rgba(0, 255, 171, 0.8)',
+        'rgba(0, 255, 171, 0.8)',
+        'rgba(0, 255, 171, 0.8)',
+        'rgba(0, 255, 171, 0.8)'],
+        backgroundColor: getRandomColors(6),
         borderWidth: 1
       }]
     },
     options: {
-        
-        
+         
       scales: {
         y: {
           beginAtZero: true
@@ -21,4 +49,31 @@
     }
   });
 
- 
+  const ctx2 = document.getElementById('Vessel').getContext('2d');
+
+  new Chart(ctx2, {
+     type: 'bar',
+     data: {
+       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+       datasets: [{
+         label: '# of Votes',
+         data: [12, 19, 3, 5, 2, 3],
+         borderColor: ['rgba(0, 255, 171, 0.8)', 
+         'rgba(0, 255, 171, 0.8)',
+         'rgba(0, 255, 171, 0.8)',
+         'rgba(0, 255, 171, 0.8)',
+         'rgba(0, 255, 171, 0.8)',
+         'rgba(0, 255, 171, 0.8)'],
+         backgroundColor: getRandomColors(6),
+         borderWidth: 1
+       }]
+     },
+     options: {
+          
+       scales: {
+         y: {
+           beginAtZero: true
+         }
+       }
+     }
+   });
